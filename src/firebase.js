3c9decbb9022053e148db43430d36b7c365f8d96
myeshco02 <mieszkoiwaniec@icloud.com>
2025-10-12 23:02:48 +0200

    feat: add Firebase integration and Tailwind CSS setup
    - Added Firebase SDK to package.json
    - Configured Firebase in src/firebase.js
    - Integrated Tailwind CSS by updating index.css and adding postcss.config.js
    - Created tailwind.config.js for Tailwind CSS configuration
    - Updated package.json to include devDependencies for Tailwind CSS and PostCSS

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDPvElPY0NlI9rW2KaeAPkAIoKBhYE2wWs",
+  authDomain: "fortune-82366.firebaseapp.com",
+  projectId: "fortune-82366",
+  storageBucket: "fortune-82366.firebasestorage.app",
+  messagingSenderId: "880932693445",
+  appId: "1:880932693445:web:53720eff94efcda0f0d7ac"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
